e40188cc4ec0649c4a469dabcadfb040f4e85fce
Mango <bilmak.sabrina@gmail.com>
2025-07-01 13:51:38 +0200

    sets

---
+++ task3.py
@@ -0,0 +1,20 @@
+def union(*args) -> set:
+    result = set()
+    for row in args:
+        for item in row:
+            result.add(item)
+    return result
+
+
+def intersect(*args) -> set:
+    result = set(args[0])
+    for row in args[1:]:
+        result = result.intersection(row)
+
+    print(result)
+
+
+union(('S', 'A', 'M'), ['S', 'P', 'A', 'C'])
+# {'S', 'P', 'A', 'M', 'C'}
+intersect(('S', 'A', 'C'), ('P', 'C', 'S'), ('G', 'H', 'S', 'C'))
+# {'S', 'C'}
