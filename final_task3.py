564e72b58c8c22c80cbbe2f0719253ec3c5276e9
Mango <bilmak.sabrina@gmail.com>
2025-07-08 13:31:05 +0200

    final

--- final_task3.py
+++ final_task3.py
@@ -1,18 +1,26 @@
 def split(data: str, sep=None, maxsplit=-1) -> list:
     result: list = []
     subresult = ""
-    for letter in data:
-        if letter == " ":
+    count: int = 0
+    if sep is None:
+        sep = " "
+    for i, letter in enumerate(data):
+        if letter == sep:
             if subresult:
                 result.append(subresult)
+                count += 1
+                if count == maxsplit:
+                    result.append(data[i:])
+                    return result
                 subresult = ""
             continue
         subresult += letter
     if subresult:
         result.append(subresult)
-    print(result)
+        count += 1
+
     return result
 
 
-split('    Hi    Python    world!', " ", -1)
+print(split('    Hi    Python    world! exit', maxsplit=2))
 # ['Hi', 'Python', 'world!']
