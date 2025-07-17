1f171798a70172693adf688a633cb4113f150b38
Sabrina Bilmak <bilmak.sabrina@gmail.com>
2025-07-17 15:09:26 +0200

    rectangle

---
+++ task7.py
@@ -0,0 +1,41 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        return self._width
+    
+    @width.setter
+    def width(self, value):
+        if value<=0:
+            raise ValueError("Value can not be lees 0")
+        self._width = value
+        
+    @property
+    def height(self):
+        return self._height
+    
+    @height.setter
+    def height(self, value):
+        if value<=0:
+            raise ValueError("Value can not be lees 0")
+        self._height = value
+        
+    @property   
+    def area(self):
+        return self._height * self._width
+    
+    @property
+    def perimeter(self):
+        return (self._height + self._width) *2
+    
+r = Rectangle(4, 3)
+print(r.area)       # ✅ 12
+print(r.perimeter)  # ✅ 14
+
+r.width = 10
+print(r.area)       # ✅ 30
+
+r.height = -5       # ❌ ValueError
